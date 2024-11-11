f6fbcfd5aec3138c7e541b3ebcb32c15f9afdd9a
Zakaria Imzilen <zakariyaimzilen@gmail.com>
2024-11-11 13:49:33 +0100

    Change Axios depending on env

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "production"
+    ? "http://learn-bs-institute.com/api/v1/"
+    : "http://localhost:3000/api/v1/";
+
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/v1/',
+  baseURL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
-
-export default axiosInstance
+export default axiosInstance;
